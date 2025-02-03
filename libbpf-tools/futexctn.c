c316890b1ec510691d9bae592b9c009f82a77742
roger_weijie <roger_weijie@realtek.com>
2025-02-03 15:57:16 +0800

    libbpf-tools: futexctn: Enforce 8 bytes alignment for eBMP map
    In eBPF subsystem, the kernel function 'stack_map_alloc' would enforce
    8-byte alignment of map size. This patch ensures that
    'perf_max_stack_depth' is rounded up to meet the 8-byte alignment
    requirement if it is not already aligned.
    
    Change-Id: I6d08fd11dc6cc9eb698a5064432b385b9be231ef
    Signed-off-by: roger_weijie <roger_weijie@realtek.com>

--- libbpf-tools/futexctn.c
+++ libbpf-tools/futexctn.c
@@ -43,7 +43,7 @@ static struct env {
 	.interval = 99999999,
 	.times = 99999999,
 	.stack_storage_size = 1024,
-	.perf_max_stack_depth = 127,
+	.perf_max_stack_depth = 128,
 };
 
 static volatile sig_atomic_t exiting = 0;
@@ -134,6 +134,11 @@ static error_t parse_arg(int key, char *arg, struct argp_state *state)
 		if (errno) {
 			fprintf(stderr, "invalid perf max stack depth: %s\n", arg);
 			argp_usage(state);
+		} else if (env.perf_max_stack_depth % 2 == 1) {
+			printf("stack depth must be a multiple of two.\n");
+			printf("convert depth from %d to %d.\n", env.perf_max_stack_depth,
+				env.perf_max_stack_depth + 1);
+			env.perf_max_stack_depth++;
 		}
 		break;
 	case OPT_STACK_STORAGE_SIZE:
