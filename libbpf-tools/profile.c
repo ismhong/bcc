c104c6c262f1574ce297bf1129dcbf30ecbb5a8a
roger_weijie <roger_weijie@realtek.com>
2025-02-03 16:34:26 +0800

    libbpf-tools: profile: Enforce 8 bytes alignment for eBPF map
    In eBPF subsystem, the kernel function 'stack_map_alloc' would enforce
    8-byte alignment of map size. This patch ensures that
    'perf_max_stack_depth' is rounded up to meet the 8-byte alignment
    requirement if it is not already aligned.
    
    Change-Id: Ia567a550a4771356e15e3c068ccdfa2317ee6310
    Signed-off-by: roger_weijie <roger_weijie@realtek.com>

--- libbpf-tools/profile.c
+++ libbpf-tools/profile.c
@@ -82,7 +82,7 @@ static struct env {
 	bool folded;
 } env = {
 	.stack_storage_size = 1024,
-	.perf_max_stack_depth = 127,
+	.perf_max_stack_depth = 128,
 	.duration = INT_MAX,
 	.freq = 1,
 	.sample_freq = 49,
@@ -208,6 +208,11 @@ static error_t parse_arg(int key, char *arg, struct argp_state *state)
 		if (errno) {
 			fprintf(stderr, "invalid perf max stack depth: %s\n", arg);
 			argp_usage(state);
+		} else if (env.perf_max_stack_depth % 2 == 1) {
+			printf("stack depth must be a multiple of two.\n");
+			printf("convert depth from %d to %d.\n", env.perf_max_stack_depth,
+				env.perf_max_stack_depth + 1);
+			env.perf_max_stack_depth++;
 		}
 		break;
 	case OPT_STACK_STORAGE_SIZE:
